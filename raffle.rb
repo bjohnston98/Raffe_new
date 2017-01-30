1cda66cc540e0af1795505661e248c7b4bb1b64d
Brian Johnston <b_johnston98@yahoo.com>
2017-01-30 11:20:19 -0500

    first pass good

--- raffle.rb
+++ raffle.rb
@@ -1,1 +1,12 @@
 #raffle.rb brand new
+
+ def raffle_function(ticket_num, winning_num_array) # want to 
+ 		#winning_num_array.include?(ticket_num)  !one way, 
+
+ 		if winning_num_array.include?(ticket_num)  #another way!
+ 			true
+ 		else
+ 			false
+ 		end
+
+end
--- test_raffle.rb
+++ test_raffle.rb
@@ -1,1 +1,21 @@
-# test_raffle.rb Brand new
+require 'minitest/autorun'
+require_relative 'raffle.rb'	#going to need a class
+
+class Raffle_class < MiniTest::Test
+
+	def test_1_equals_1		# a test to test runninb
+		assert_equal(1,1)
+	end
+
+	def test_number_exists 	#1sttest
+		ticket_num = '1'
+		winning_num_array = []
+		assert_equal(false, raffle_function(ticket_num, winning_num_array))	#one way to build array
+	end
+
+	def test_returns_true
+		ticket_num = '1234'  #some ticket num
+		winning_num_array = ['1234']
+		assert_equal(true, raffle_function(ticket_num, winning_num_array))
+	end	
+end
